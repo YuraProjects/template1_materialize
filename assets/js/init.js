de2ef279b243b2c9aaab4caa19c6e5c5102c5024
YuraProjects <devonwullan@gmail.com>
2021-08-10 01:37:47 +0800

    First Commit

---
+++ assets/js/init.js
@@ -0,0 +1,16 @@
+$(document).ready(function () {
+	$(".button-collapse").sideNav();
+	$(".slider").slider({
+
+		indicators: false,
+
+		height: 500,
+
+		transition: 600,
+
+		interval: 6500
+
+	});
+	$('.parallax').parallax();
+	$('.materialboxed').materialbox();
+});
